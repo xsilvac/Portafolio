26beffa1c84d901c13117caa4fddfa684c201e1d
ximena silva <ximenasilvacotrina11@gmail.com>
2022-08-16 11:34:50 -0500

    Boton para descargar cv en PDF y terminar el Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import {NavLink}  from 'react-router-dom';
+import {NavLink, Link}  from 'react-router-dom';
+
 
 const NavBar = () => {
   return (
     <div>
-        <nav id="navbar" className="navbar navbar-expand-lg navbar-dark">
-            <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">About me</NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="navbar-brand">
-                    <NavLink className="nav-link active"  aria-current="page" to="/">Proyectos</NavLink>
-                    </li>
-                    <li className="navbar-brand">
-                    <NavLink className="nav-link" to="/">Tecnologias</NavLink>
-                    </li>
-                    <li className="navbar-brand">
-                    <NavLink className="nav-link" to="/">Contacto</NavLink>
-                    </li>
-                </ul>
-                </div>
+      <nav id="navbar" className="navbar navbar-expand-lg navbar-dark">
+        <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">About me</NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="navbar-brand">
+                <NavLink className="nav-link active"  aria-current="page" to="/">Proyectos</NavLink>
+              </li>
+              <li className="navbar-brand">
+                <NavLink className="nav-link" to="/">Tecnologias</NavLink>
+              </li>
+              <li className="navbar-brand">
+                <NavLink className="nav-link" to="/">Contacto</NavLink>
+              </li>
+            </ul>
+            <div className="me-5">
+              <NavLink className="btnCV" to="./XimenaSilvaCV.pdf" download target="_blank">CV</NavLink>
             </div>
-        </nav>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
