35f43058a9290b3436f6f40c8b87751298d8bc76
ximena silva <ximenasilvacotrina11@gmail.com>
2022-08-18 17:56:20 -0500

    Mi presentación con diseño de maquina de escribir

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="title">
+      <Typewriter
+       onInit={(typewriter)=> {
+       typewriter
+       .typeString("Hola, mi nombre es <strong> Ximena Silva ... </strong> <br/>")
+       .pauseFor(1000)
+       .typeString("Soy front end developer")
+       .start();
+       }}
+       />
+    </div>
   )
 }
 
