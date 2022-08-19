f7cf9488e8dde32d047b8da368f24cedded14964
ximena silva <ximenasilvacotrina11@gmail.com>
2022-08-18 23:07:37 -0500

    Porcentajes de mis habilidades tecnicas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Typewriter from "typewriter-effect";
+import Skills from './Skills';
+import data from '../data/data.json'
+
 
 const Home = () => {
+ // console.log(data)
+  const [bd, setBd] = useState([]);
+  
+  useEffect(() => {
+    setBd(data.skills)
+    console.log(bd)
+  }, [])
+  
   return (
     <div className="title">
       <Typewriter
-       onInit={(typewriter)=> {
-       typewriter
-       .typeString("Hola, mi nombre es <strong> Ximena Silva ... </strong> <br/>")
-       .pauseFor(1000)
-       .typeString("Soy front end developer")
-       .start();
-       }}
-       />
+        onInit={(typewriter)=> {
+        typewriter
+        .typeString("Hola, mi nombre es <strong> Ximena Silva ... </strong> <br/>")
+        .pauseFor(1000)
+        .typeString("Soy front end developer")
+        .start();
+        }}
+        />
+      {bd.map((info) =>
+        <Skills technology={info.technology} percent={info.percent}/>
+      )}
+      
     </div>
   )
 }
